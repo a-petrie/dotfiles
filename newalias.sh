df5f63aff8d5569b36c30c680d9486ff2023b0ad
a-petrie <allantpetrie@googlemail.com>
2021-12-07 11:23:23 +0000

    fixing error in new alias script

--- newalias.sh
+++ newalias.sh
@@ -7,5 +7,5 @@ echo Alias command:
 read command
 
 echo alias $alias=\'$command\' >> ~/dotfiles/bash_aliases
-cp ~/dotfiles/bashaliases ~/.bash_aliases
+cp ~/dotfiles/bash_aliases ~/.bash_aliases
 source ~/.bash_aliases
